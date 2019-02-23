ccd16675c79c6da9879008ff6dad59a66783414a
MT-01 <mt-01@mt-01s-imac.local>
2019-02-23 18:25:39 +0530

    Add label on storyboard

--- GoogleSignIn/ViewController.swift
+++ GoogleSignIn/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
